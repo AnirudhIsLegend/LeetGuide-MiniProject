d7b0a4da651c0710a8790fa7ea4d36db5d4343d6
AnirudhIsLegend <140842049+AnirudhIsLegend@users.noreply.github.com>
2025-11-15 19:14:37 +0530

    Delete LeetGuide/background.js

--- LeetGuide/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === "GET_GEMINI_KEY") {
-    chrome.storage.local.get("geminiApiKey", data => {
-      sendResponse({ key: data.geminiApiKey || null });
-    });
-    return true; // keep channel open for async sendResponse
-  }
-});
